fff9d9b3089a76f1f240c42971d4a912c5149ed1
Shalini Shekhar <shalini.ichha@gmail.com>
2022-08-03 04:40:48 -0400

    made changes for website, added design, theme, pages

--- app.py
+++ app.py
@@ -23,6 +23,42 @@ def index():
         jsonData = json.dumps(x, indent=4, default=json_util.default)
     return render_template("index.html", jsonData=jsonData)
 
+@app.route('/currencies', methods=['GET'])
+def getAllCurrencies():
+    project={
+        'rates': 1,
+        '_id': 0
+        }
+    result = client['currency']['currency'].find(
+        projection=project
+    )
+    for x in result:
+        jsonData = json.dumps(x, indent=4, default=json_util.default)
+        countryNames = ','.join(list(json.loads(jsonData)['rates'].keys()))
+    if(request.method == "GET"):
+        return render_template('currencies.html',jsonData=jsonData)
 
+@app.route('/conversion', methods=['POST', 'GET'])
+def conversion():
+    project={
+        'rates': 1,
+        '_id': 0
+        }
+    result = client['currency']['currency'].find(
+        projection=project
+    )
+    for x in result:
+        jsonData = json.dumps(x, indent=4, default=json_util.default)
+        countryNames = ','.join(list(json.loads(jsonData)['rates'].keys()))
+    if(request.method == "GET"):
+        print(countryNames)
+        return render_template('conversion.html', countryNames=countryNames)
+    if(request.method == "POST"):
+        data = json.loads(jsonData)['rates']
+        baseCurrency = request.form['baseCurrency']
+        conversionCurrency = request.form['conversionCurrency']
+        baseAmount = request.form['baseAmount']
+        convertedAmount = (data[conversionCurrency] / data[baseCurrency]) * float(baseAmount)
+        return render_template('conversion.html', countryNames=countryNames, baseCurrency=baseCurrency, conversionCurrency=conversionCurrency, baseAmount=baseAmount, convertedAmount=convertedAmount)
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
