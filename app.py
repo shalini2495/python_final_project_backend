fd2c507be03f569d8b8b5d08523df1ff3843e0ab
Shalini Shekhar <shalini.ichha@gmail.com>
2022-08-09 23:51:28 -0400

    added code to get the data and save in mongo

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+from xml.etree.ElementInclude import include
 from flask import Flask, jsonify, request, render_template
+from pkg_resources import require
 from pymongo import MongoClient
 import urllib
+import requests
+import schedule
 from dotenv import load_dotenv
 import json
 from bson import json_util
 import os
+from datetime import date
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 load_dotenv()
 app = Flask(__name__, template_folder='templates')
@@ -13,16 +20,35 @@ user = os.getenv("MONGODB_USER")
 password = os.getenv("MONGODB_PASS")
 client = MongoClient(f"mongodb://{user}:{urllib.parse.quote(password)}@ac-qcg93v9-shard-00-00.wjt7sxv.mongodb.net:27017,ac-qcg93v9-shard-00-01.wjt7sxv.mongodb.net:27017,ac-qcg93v9-shard-00-02.wjt7sxv.mongodb.net:27017/?ssl=true&replicaSet=atlas-lqwqhj-shard-0&authSource=admin&retryWrites=true&w=majority", connect=False)
 db = client.currency
+def getData():
+    countries = os.path.join(SITE_ROOT, "static/data", "countries.json")
+    data = json.load(open(countries))
+    stringData = ",".join(data)
+    
+    r = requests.get(f'https://api.exchangerate.host/latest?base=USD&symbols={stringData})')
+    if r.status_code == 200:
+        ExchangeRateData = r.json()
+        print(ExchangeRateData)
+        db.currency.insert_one({
+            "rates": ExchangeRateData["rates"],
+            "date": ExchangeRateData["date"],
+            "base": ExchangeRateData["base"]
+        })
+schedule.every().day.at("01:00").do(getData)
+getData()
+
 
 @app.route('/')
 def index():
-    data = db.currency.find({})
+    currentDate = date()
+    formatedDate = currentDate.strftime("%Y-%m-%d")
+    data = db.currency.find({"date": formatedDate})
     jsonData = {}
     for x in data:
         jsonData = json.dumps(x, indent=4, default=json_util.default)
     return render_template("index.html", jsonData=jsonData)
 
-@app.route('/currencies', methods=['GET'])
+@app.route('/currencies')
 def getAllCurrencies():
     project={
         'rates': 1,
@@ -33,10 +59,8 @@ def getAllCurrencies():
     )
     for x in result:
         jsonData = json.dumps(x, indent=4, default=json_util.default)
-        countryNames = ','.join(list(json.loads(jsonData)['rates'].keys()))
-    if(request.method == "GET"):
-        return render_template('currencies.html',jsonData=jsonData)
-
+    return render_template('currencies.html',jsonData=jsonData)
+    
 @app.route('/conversion', methods=['POST', 'GET'])
 def conversion():
     project={
